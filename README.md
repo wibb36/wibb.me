b517eb258587c6604c9c1aac071d6050e7d8e99e
Sam Wibb <samwibb@sams-mbp.lan>
2023-04-12 14:52:07 -0700

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# wibb.me
+
+The official source code for [wibb.me](https://wibb.me/). which contains information about me, my skills, my projects, blog posts, and much more.
+
+> In 2023, I updated my site using Next.js 13's new app/ directory.
+
+## Stack
+
+-   **Framework**: [Next.js](https://nextjs.org/)
+-   **Deployment**: [Vercel](https://vercel.com)
+-   **Styling**: [Tailwind CSS](https://tailwindcss.com)
+-   **Analytics**: [Vercel Analytics](https://vercel.com/analytics)
+
+## Running Locally
+
+This application requires Node.js v16.13+.
+
+```bash
+git clone https://github.com/wibb36/wbb.me.git
+cd wibb.me
+pnpm install
+pnpm dev
+```
+
+## License
+
+Please review the [license](https://github.com/wibb36/wibb.me/blob/main/LICENSE.txt) and remove all of my personal information.
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            typography: {
+                quoteless: {
+                    css: {
+                        'blockquote p:first-of-type::before': {
+                            content: 'none',
+                        },
+                        'blockquote p:first-of-type::after': {
+                            content: 'none',
+                        },
+                    },
+                },
+            },
+            colors: {
+                primary: '#2cffd1cb',
+                error: '#ff2c2c',
+            },
+        },
+    },
+    plugins: [require('@tailwindcss/typography')],
+}
